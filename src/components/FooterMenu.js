a29d97044773c5aae1e4de0d4b847d4ad65b0016
vagnerpavani <vagner.alves.pavani@gmail.com>
2021-05-07 13:37:34 -0300

    preparando review

--- src/components/FooterMenu.js
+++
@@ -1,9 +0,0 @@
-import Menu from "./Menu";
-export default function FooterMenu() {
-  return (
-    <Menu
-      menuClass="footer-menu"
-      iconNames="home search-outline add-circle-outline heart-outline person-outline"
-    />
-  );
-}
--- src/components/SuggestionsHeader.js
+++
@@ -1,8 +0,0 @@
-export default function SuggestionsHeader() {
-  return (
-    <div class="suggestion">
-      <p> Sugestões para você </p>
-      <a href="#">Ver tudo</a>
-    </div>
-  );
-}
